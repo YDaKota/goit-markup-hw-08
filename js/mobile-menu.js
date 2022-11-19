9ae7e61b3ae3bb99293f0af304e271f2325360b6
YDaKota <95923839+YDaKota@users.noreply.github.com>
2022-11-19 10:58:05 +0200

    add mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -31,16 +31,50 @@
 
 
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const menuBtnOpen = document.querySelector("[data-menu-open]");
+  const menuBtnClose = document.querySelector("[data-menu-close]");
   const mobileMenuRef = document.querySelector("[data-menu]");
 
-  menuBtnRef.addEventListener("click", () => {
+  menuBtnOpen.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnOpen.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnOpen.classList.toggle("is-open");
+    menuBtnOpen.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+  menuBtnClose.addEventListener("click", () => {
+    const expanded =
+      menuBtnClose.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnClose.classList.toggle("is-open");
+    menuBtnClose.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
+
+
+/*
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-list'),
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
+  }
+})();
+*/
